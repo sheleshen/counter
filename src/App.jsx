808ecc2d5f7d5acd8d0275ff084495bcb82b7894
sheleshen <sheleshnewaanna@gmail.com>
2023-11-15 18:54:22 +0300

    Первичный

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import "./App.css";
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Проверка связи!
+      </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim maiores in, fugiat mollitia, excepturi temporibus facilis labore nulla doloremque vero corporis nisi architecto eligendi, deserunt consequatur sint sunt blanditiis atque.
+      </p>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+    return(
+        <button className="mx-10 bg-blue-400 rounded text-lg px-4 py-2 text-white font-bold">
+            Привет
+        </button>
+    )
+}
+
+export default Button
